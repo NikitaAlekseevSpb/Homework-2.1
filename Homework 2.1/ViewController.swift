87d08d5f2aa2d77cdf4bd20f8bc084d82a2b352f
Nik <nik.alekseev.spb@gmail.com>
2021-03-25 19:23:35 +0300

    Finished work

---
+++ Homework 2.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework 2.1
+//
+//  Created by MacBook on 25.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
